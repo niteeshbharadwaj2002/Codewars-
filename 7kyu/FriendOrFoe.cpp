e3a551c6b60195da2f62c6ca487a0a73a3ee3546
niteeshbharadwaj2002 <niteeshbharadwaj2021@gmail.com>
2026-01-28 15:51:02 +0100

    Create FriendOrFoe.cpp

---
+++ 7kyu/FriendOrFoe.cpp
@@ -0,0 +1,26 @@
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+// Input = ["Ryan", "Kieran", "Jason", "Yous"]
+// Output = ["Ryan", "Yous"]
+
+// Input = ["Peter", "Stephen", "Joe"]
+// Output = []
+// Input strings will only contain letters.
+// Note: keep the original order of the names in the output.
+
+//SOLUTION HERE
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<string> friendOrFoe(const vector<string>& input) {
+    // Good luck!
+    vector<string> friends;
+    
+    for (string name : input){
+      if (name.length()==4) {friends.push_back(name);}
+    }
+    return friends;
+}
